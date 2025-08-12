40a0a0a1c28570634ae55f89fc717e6783a86429
fixlaggame23-prog <fixlaggame23@gmail.com>
2025-08-12 15:39:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// script.js - companion JS for UltraShare package
+// The main behavior is embedded in index.html. Use this file to add server integration, analytics,
+// or to move the inline scripts from index.html here for cleaner separation.
+console.log("UltraShare companion script loaded.");
